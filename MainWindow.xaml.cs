1546ec89f862621bccc6a00391f46b7fed2a2c29
ahalcyon <ito.d.af@m.titech.ac.jp>
2019-05-26 17:15:41 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalcGUI
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static readonly string FailMessage = "計算失敗";
+        static decimal num = 0;
+        static string inKey = null;
+        static string ope = null;
+        static decimal result = 0;
+        static decimal d = 0;
+        public void button0_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "0";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button1_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "1";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button2_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "2";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button3_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "3";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button4_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "4";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button5_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "5";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button6_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "6";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button7_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "7";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button8_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "8";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void button9_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "9";
+            if (decimal.TryParse(formula.Text, out d))
+            {
+                formula.Text = (System.Convert.ToDecimal(formula.Text) * 10 + System.Convert.ToDecimal(inKey)).ToString();
+            }
+            else
+            {
+                formula.Text = inKey;
+            }
+        }
+
+        public void buttonP_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "+";
+            if (formula.Text == FailMessage)
+            {
+                formula.Text = "0";
+            }
+            else if (ope == null)
+            {
+                result = System.Convert.ToDecimal(formula.Text);
+                ope = inKey;
+                formula.Text = inKey;
+            }
+            /*else
+            {
+                Calc();
+                formula.Text = ope;
+                Clear();
+            }*/
+        }
+        public void buttonM_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "-";
+            if (formula.Text == FailMessage)
+            {
+                formula.Text = "0";
+            }
+            else if (ope == null)
+            {
+                result = System.Convert.ToDecimal(formula.Text);
+                ope = inKey;
+                formula.Text = inKey;
+            }
+            /*else
+            {
+                Calc();
+                formula.Text = ope;
+                Clear();
+            }*/
+        }
+        public void buttonD_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "/";
+            if (formula.Text == FailMessage)
+            {
+                formula.Text = "0";
+            }
+            else if (ope == null)
+            {
+                result = System.Convert.ToDecimal(formula.Text);
+                ope = inKey;
+                formula.Text = inKey;
+            }
+            /*else
+            {
+                Calc();
+                formula.Text = ope;
+                Clear();
+            }*/
+        }
+        public void buttonT_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "*";
+            if (formula.Text == FailMessage)
+            {
+                formula.Text = "0";
+            }
+            else if (ope == null)
+            {
+                result = System.Convert.ToDecimal(formula.Text);
+                ope = inKey;
+                formula.Text = inKey;
+            }
+            /*else
+            {
+                num = System.Convert.ToDecimal(formula.Text);
+                Calc();
+                formula.Text = ope;
+                Clear();
+            }*/
+        }
+        public void buttonAC_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "AC";
+            formula.Text = "0";
+            Clear();
+        }
+        public void buttonE_Click(object sender, RoutedEventArgs e)
+        {
+            inKey = "=";
+            if (formula.Text == FailMessage)
+            {
+                formula.Text = "0";
+            }
+            else if (ope == null)
+            {
+                formula.Text = "0";
+            }
+            else
+            {
+                num = System.Convert.ToDecimal(formula.Text);
+                Calc();
+                formula.Text = ope;
+                Clear();
+            }
+        }
+        public static void Calc()
+        {
+            switch (ope)
+            {
+                case "+":
+                    result = result + num;
+                    break;
+                case "-":
+                    result = result - num;
+                    break;
+                case "*":
+                    result = result * num;
+                    break;
+                case "/":
+                    if (num != 0)
+                    {
+                        result = result / num;
+                    }
+                    else
+                    {
+                        result = 0;
+                    }
+                    break;
+                case "^":
+                    result = Powd(result, num);
+                    break;
+            }
+            
+                ope=result.ToString();
+            
+        }
+        public static void Clear()
+        {
+            inKey = null;
+            num = 0;
+            ope = null;
+            result = 0;
+            d = 0;
+        }
+        public static decimal Powd(decimal a, decimal b)
+        {
+            if (b == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * Powd(a, b - 1);
+            }
+        }
+    }
+}
